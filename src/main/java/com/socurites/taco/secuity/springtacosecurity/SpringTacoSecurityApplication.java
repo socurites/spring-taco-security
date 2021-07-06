7e3ef4a25e12221b32627464e1e239176d18845d
Junyi Song <junyisong@this.local>
2021-07-06 11:03:30 +0900

    First Commit

---
+++ src/main/java/com/socurites/taco/secuity/springtacosecurity/SpringTacoSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.socurites.taco.secuity.springtacosecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTacoSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTacoSecurityApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/socurites/taco/secuity/springtacosecurity/SpringTacoSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socurites.taco.secuity.springtacosecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTacoSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
